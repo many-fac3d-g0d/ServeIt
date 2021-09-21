2f0d69a55242027eb76cf479b74ce2424d8041c7
Vignesh Nandakumar <nviki001@gmail.com>
2021-09-21 22:14:22 +0530

    Major changes - 1. Added start/stop buttons 2. AppBar 3. Alignment changes 4.logo

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,16 @@ class Home extends StatefulWidget{
 }
 
 class _HomeState extends State<Home>{
-  String statusText = "😎";
+  String statusText = "";
   String? wifiName, wifiIPv4;
   String baseDir = "/sdcard/Download";
   //default port to 8888
   int portNo = 8888;
   String serverUrl = "http://";
+  bool canStartServer = true;
+  bool canStopServer = false;
+  var myserver;
+  
 
   final NetworkInfo _networkInfo = NetworkInfo();
   final dirController = TextEditingController();
@@ -85,6 +89,7 @@ class _HomeState extends State<Home>{
 
     try {
       wifiIPv4 = await _networkInfo.getWifiIP();
+      debugPrint('Inside _initNetworkInfo() wifiIPv4 : ${wifiIPv4.toString()}');
     } on Exception catch (e) {
       print(e.toString());
     }
@@ -92,6 +97,7 @@ class _HomeState extends State<Home>{
 
   startServer() async{
 
+    await _initNetworkInfo();
     debugPrint("User inputted dir ${dirController.text}");
     debugPrint("User inputted port ${portController.text}");
 
@@ -103,98 +109,115 @@ class _HomeState extends State<Home>{
 
 
     setState((){
-      statusText = "Starting server on port : "+portNo.toString();
+      //statusText = "Starting server on port : "+portNo.toString();
       debugPrint('Inside startServer()');
     });
-if (await Permission.storage.request().isGranted) {
-    debugPrint('is wifiName there ? : $wifiName');
-    if(wifiName != null){ // wifiName detected start the server 
-      HttpServer
-      .bind(wifiIPv4.toString(), portNo)
-      .then((server) {
-        setState(() {
-            statusText = "Server successfully started on "+wifiIPv4.toString()+":"+portNo.toString();
-            serverUrl = serverUrl+wifiIPv4.toString()+":"+portNo.toString();
-        });
-        server.listen((HttpRequest request) async{
-          debugPrint('Received request ${request.method}: ${request.uri.path}');
-          switch(request.method){
-            case 'GET':
-              String currDir = '';
-              if(request.uri.path == '/')//For first GET add base url to '/'
-                currDir = baseDir + Uri.decodeFull(request.uri.path);
-              else
-                currDir = Uri.decodeFull(request.uri.path);
-              //If request is for a file, send the file in response to client
-              if(File(currDir).existsSync()){
-                File downloadFile = getFile(currDir);
-                var sink = downloadFile.openRead();
-                String fileName = Uri.decodeFull(request.uri.path);
+  if (await Permission.storage.request().isGranted) {
+      debugPrint('is wifiName there ? : $wifiName');
+      debugPrint('wifiIPv4 : ${wifiIPv4.toString()}');
+      if(wifiName != null){ // wifiName detected start the server 
+        HttpServer
+        .bind(wifiIPv4.toString(), portNo)
+        .then((server) {
+          setState(() {
+              myserver = server;// server instance will be required for stopServer()
+              statusText = "Server started on http://"+wifiIPv4.toString()+":"+portNo.toString();
+              serverUrl = serverUrl+wifiIPv4.toString()+":"+portNo.toString();
+              canStartServer = false;
+              canStopServer = true;
+          });
+          server.listen((HttpRequest request) async{
+            debugPrint('Received request ${request.method}: ${request.uri.path}');
+            switch(request.method){
+              case 'GET':
+                String currDir = '';
+                if(request.uri.path == '/')//For first GET add base url to '/'
+                  currDir = baseDir + Uri.decodeFull(request.uri.path);
+                else
+                  currDir = Uri.decodeFull(request.uri.path);
+                //If request is for a file, send the file in response to client
+                if(File(currDir).existsSync()){
+                  File downloadFile = getFile(currDir);
+                  var sink = downloadFile.openRead();
+                  String fileName = Uri.decodeFull(request.uri.path);
 
-                List filePath = fileName.split('/');//Get filename from request path
-                fileName = filePath.last;
+                  List filePath = fileName.split('/');//Get filename from request path
+                  fileName = filePath.last;
 
-                fileName = fileName.replaceAll(new RegExp(r'[^a-zA-Z0-9.]'), '_');//Sanitize filename to send in response
+                  fileName = fileName.replaceAll(new RegExp(r'[^a-zA-Z0-9.]'), '_');//Sanitize filename to send in response
 
-                request.response.headers.add("Content-Disposition", "attachment;  filename=$fileName");
-                await request.response.addStream(sink);
-                request.response.flush();
-                request.response.close();
-                
-                debugPrint("File download: $downloadFile");
-              }
-
-              //If request is for a directory, add a link so that user can access the directory
-              else if(Directory(currDir).existsSync()){
-                String baseResponse = "<html><head><h1><p>Directory listing</p></h1></head><body>";
-                List dirFiles = getDir(currDir);
-                for(var i=0; i<dirFiles.length; i++){
-                  List fileNamePath = dirFiles[i].toString().split('/');
-                  String fileName = fileNamePath.last.toString();
-                  String fileOrDir = fileNamePath.first.toString();
-                  fileName = fileName.substring(0,fileName.length - 1);
-
-                  if(fileOrDir.contains('File')) //Current item is a File
-                    baseResponse = baseResponse + '<li><a href="${currDir+fileName}">$fileName</a>';
-                  else //Current item is a directory
-                    baseResponse = baseResponse + '<li><a href="${currDir+fileName+'/'}">$fileName</a>';
+                  request.response.headers.add("Content-Disposition", "attachment;  filename=$fileName");
+                  await request.response.addStream(sink);
+                  request.response.flush();
+                  request.response.close();
+                  
+                  debugPrint("File download: $downloadFile");
                 }
-                baseResponse = baseResponse + '</body></html>';
-                request.response.headers.contentType =new ContentType('text','html',charset : 'utf-8');
-                request.response.write(baseResponse);
-                request.response.close();
-                debugPrint("Directory download: $dirFiles");
-              }
-              // Not a file or directory can be read from filesystem throw error
-              else{
-                debugPrint("Error reading File/Directory");
-                request.response.write('Error reading File/Directory ${request.method}: $currDir ');
-                request.response.close();
-              }
-              
-              break;
-            default:
-              request.response.write('Cannot ${request.method}: ${request.uri.path} ');
-              request.response.close();
-              break;
 
-            }
+                //If request is for a directory, add a link so that user can access the directory
+                else if(Directory(currDir).existsSync()){
+                  String baseResponse = "<html><head><h1><p>Directory listing</p></h1></head><body>";
+                  List dirFiles = getDir(currDir);
+                  for(var i=0; i<dirFiles.length; i++){
+                    List fileNamePath = dirFiles[i].toString().split('/');
+                    String fileName = fileNamePath.last.toString();
+                    String fileOrDir = fileNamePath.first.toString();
+                    fileName = fileName.substring(0,fileName.length - 1);
+
+                    if(fileOrDir.contains('File')) //Current item is a File
+                      baseResponse = baseResponse + '<li><a href="${currDir+fileName}">$fileName</a>';
+                    else //Current item is a directory
+                      baseResponse = baseResponse + '<li><a href="${currDir+fileName+'/'}">$fileName</a>';
+                  }
+                  baseResponse = baseResponse + '</body></html>';
+                  request.response.headers.contentType =new ContentType('text','html',charset : 'utf-8');
+                  request.response.write(baseResponse);
+                  request.response.close();
+                  debugPrint("Directory download: $dirFiles");
+                }
+                // Not a file or directory can be read from filesystem throw error
+                else{
+                  debugPrint("Error reading File/Directory");
+                  request.response.write('Error reading File/Directory ${request.method}: $currDir ');
+                  request.response.close();
+                }
+                
+                break;
+              default:
+                request.response.write('Cannot ${request.method}: ${request.uri.path} ');
+                request.response.close();
+                break;
+
+              }
+          });
         });
+      }
+      else// No wifi connection detected no point in server start 
+          statusText = "No wifi connection detected, please connect to a wifi network";
+
+    }else{
+      setState(() {
+        statusText = "Need file storage permissions to serve files";
       });
     }
-    else// No wifi connection detected no point in server start 
-        statusText = "No wifi connection detected, please connect to a wifi network to start server";
 
-  }else{
-    setState(() {
-      statusText = "Need file storage permissions to serve files";
+  }
+
+  stopServer(){
+    myserver.close(force : true);
+    setState(() {// invoke widget build and change setState()
+      canStartServer = true;
+      canStopServer = false;
+      statusText = "Server stopped";
     });
   }
 
-  }
   @override
     Widget build(BuildContext context){
       return Scaffold(
+        appBar: AppBar(
+          title: const Text('ServeIt')
+         ),
         body: SingleChildScrollView(
         child: ConstrainedBox(
           constraints: BoxConstraints(),
@@ -203,28 +226,45 @@ if (await Permission.storage.request().isGranted) {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                TextField(
-                  controller: dirController,
-                  decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter the directory path'
+                Text("Enter the directory path to share :"),
+                Container(
+                    width: 300.0,
+                    child: TextField(
+                      controller: dirController,
+                      decoration: const InputDecoration(
+                      border: UnderlineInputBorder()
+                      )
                   )
                 ),
-                TextField(
-                  controller: portController,
-                  decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),
-                  labelText: 'Enter the port no'
+                Text("Enter the port no :"),
+                Container(
+                    width: 40.0,
+                    child: TextField(
+                    controller: portController,
+                    decoration: const InputDecoration(
+                    border: UnderlineInputBorder()
+                    )
                   )
                 ),
-                ElevatedButton(
-                  onPressed: (){
-                    startServer();
-                  },
-                  child: Text("Start Server"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children : <Widget> [ ElevatedButton(
+                      onPressed: !canStartServer ? null : (){ // Disable Start Server button based on flag
+                        startServer();
+                      },
+                      child: Text("Start Server")
+                    ),
+                    ElevatedButton(
+                      onPressed: !canStopServer ? null : (){
+                        stopServer();
+                      },
+                       child: Text("Stop Server"))
+                  ]
                 ),
                 Text(statusText),
-                QrImage(data: serverUrl)
+                QrImage(data: serverUrl),
+                Text("Copyright Viki Inc 2021")
               ],
             ),
             
